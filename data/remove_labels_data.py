88649a165a5de252de65adf4b8424866227fc79e
Didi Zhou <didizhou1221@gmail.com>
2021-04-12 15:41:46 -0500

    unlabelled data

--- data/remove_labels_data.py
+++ data/remove_labels_data.py
@@ -1,20 +1,26 @@
 
-import pandas as pd
+# import pandas as pd
 
-# 3 -> no quoting
-movies_data = pd.read_csv("data/movies.txt", delimiter=",", quoting=0)
+# # 3 -> no quoting
+# movies_data = pd.read_csv("data/movies.txt", delimiter=",", quoting=0)
 
-# recode rationales so that sentences are (1) or are not (-1)
-# rationales; polarity is implicit
-sent_lbls = movies_data["sentence_lbl"]
-# in the original dataset: positive rationale is 2, the negative 
-#  rationale is 0, and the neutral one is 1. 
-sent_lbls_recoded = sent_lbls.replace(1, -1) 
-# sent_lbls_recoded = sent_lbls_recoded.replace([1,-1], 1) 
-movies_data["sentence_lbl"] = sent_lbls_recoded
+# # recode rationales so that sentences are (1) or are not (-1)
+# # rationales; polarity is implicit
+# sent_lbls = movies_data["sentence_lbl"]
+# # in the original dataset: positive rationale is 2, the negative 
+# #  rationale is 0, and the neutral one is 1. 
+# sent_lbls_recoded = sent_lbls.replace(1, -1) 
+# # sent_lbls_recoded = sent_lbls_recoded.replace([1,-1], 1) 
+# movies_data["sentence_lbl"] = sent_lbls_recoded
 
-movies_data.to_csv("data/movies.txt", index=False)
-
-
-
-
+# movies_data.to_csv("data/movies.txt", index=False)
+    
+with open('data/movies_labelled.txt') as infile, open('data/movies_unlabelled.txt', 'w') as outfile:
+    for line in infile:
+        words = line.rsplit(',', 1)
+        if len(words) > 1:
+            words[1] = '-1'
+        if len(words) > 1:
+            outfile.write(words[0] + "," + words[1] + "\n")
+        else:
+            outfile.write(words[0] + "\n")
